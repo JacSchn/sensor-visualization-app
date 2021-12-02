e964ff34ad82dd1cbb998b0c58b6b0779a71a857
GasserJC <jacobgasser17@gmail.com>
2021-12-01 19:20:40 -0600

    aye db is goin

--- functions/firestore.js
+++ functions/firestore.js
@@ -1,7 +1,7 @@
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
 
-const serviceAccount = require('./creds/creds.firestore.json');
+const serviceAccount = require('../creds/creds.firestore.json');
 
 initializeApp({
     credential: cert(serviceAccount)
@@ -33,12 +33,8 @@ const SaveHistorical = async (sensorName, keyData, valueData, timestamp) => {
     if (sensorName === undefined || sensorName === null || timestamp === undefined || timestamp === null) {
         throw "Name and Timestamp are mandetory";
     }
-    if (!(sensorName instanceof string)) {
-        throw "sensorName must be string."
-    }
-    if (!(timestamp instanceof string)) {
-        throw "timestamp must be string."
-    }
+    sensorName = String(sensorName)
+    timestamp = String(timestamp)
     if (keyData !== undefined || keyData !== null || valueData !== undefined || valueData !== null) {
         if (!(valueData instanceof Array) || !(keyData instanceof Array)) {
             throw "keyData and valueData must be an array if populated";
@@ -62,13 +58,33 @@ const SaveHistorical = async (sensorName, keyData, valueData, timestamp) => {
     }
 }
 
-SaveStatus("LED-Fixture","On",['example','of','misc'])
+const GetHistorical = async (sensorName) => {
+    const db = await getFirestore();
+    let returnData = []
+    const snapshot = await db.collection(sensorName).get();
+    snapshot.forEach((doc) => {
+        returnData.push(doc.data());
+    })
+    console.log(returnData)
+    return returnData
+}
+
+const GetStatus = async (sensorName) => {
+    const db = await getFirestore();
+    let returnData = []
+    const snapshot = await db.collection('SensorStatus').get(sensorName);
+    snapshot.forEach((doc) => {
+        returnData.push(doc.data());
+    })
+    console.log(returnData)
+    return returnData
+}
 
 module.exports = {
     SaveHistorical: SaveHistorical,
     SaveStatus: SaveStatus,
-    //GetHistorical: GetHistorical,
-    //GetStatus: GetStatus,
+    GetHistorical: GetHistorical,
+    GetStatus: GetStatus,
     //DeleteHistorical: DeleteHistorical,
     //DeleteStatus: DeleteStatus
 }
